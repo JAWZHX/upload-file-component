b3c1c815c38c9ac40da8bb94bc94aa9568678112
JAWZHX <1219251709@qq.com>
2018-11-12 18:15:55 +0800

    1、搭建静态服务器

---
+++ src/server/index.js
@@ -0,0 +1,18 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const path = require('path');
+
+const app = new Koa();
+const router = new Router();
+
+// 搭建静态服务器
+app.use(require('koa-static')(path.resolve(__dirname, '../pages')));
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+
+app.listen(8080, '0.0.0.0', () => {
+  console.log('服务器运行在http://localhost:8080');
+});
